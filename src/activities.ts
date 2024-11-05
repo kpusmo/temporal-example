1781320abe40520960c757384ce75ebb1b760c1e
Krzysztof Smolarkiewicz <kpusmo@gmail.com>
2024-11-06 00:48:21 +0100

    keep people data between workflow runs

--- src/activities.ts
+++ src/activities.ts
@@ -1,10 +1,7 @@
-import { SearchRule, SwPerson } from './types';
+import { GetPeopleRequest, getPeopleSignal, SearchRule, SwPerson } from './types';
 import axios, { AxiosResponse } from 'axios';
-import { ApplicationFailure } from '@temporalio/client';
-
-export async function greet(name: string): Promise<string> {
-  return `Hello, ${name}!`;
-}
+import { ApplicationFailure, Client } from '@temporalio/client';
+import { activityInfo } from '@temporalio/activity';
 
 interface SwResponse {
   count: number;
@@ -13,35 +10,57 @@ interface SwResponse {
   results: SwPerson[];
 }
 
-export async function fetchPeople(swApiUrl: string): Promise<SwPerson[]> {
-  const fetchPage = (pageUrl: string) => axios.get(pageUrl);
-  const processPage = async ({ data: pageResult }: AxiosResponse<SwResponse>): Promise<SwPerson[]> => {
-    // loading everything to memory with recursive calls
-    // there are 82 items in SW api, so that's ok, but with more data we should persist it to some external storage instead
-    // (and refactor to iterative manner)
-    if (pageResult.next) {
-      return [...pageResult.results, ...(await fetchPage(pageResult.next).then(processPage))];
-    }
-    return pageResult.results;
-  };
-  const results = await fetchPage(`${swApiUrl}/people`).then(processPage);
-  return results;
-}
+export function createActivities(client: Client) {
+  return {
+    async greet(name: string): Promise<string> {
+      return `Hello, ${name}!`;
+    },
 
-export async function filterPeople(people: SwPerson[], rules: SearchRule<SwPerson>[]): Promise<SwPerson[]> {
-  return rules.reduce(
-    (filteredPeople, rule) =>
-      filteredPeople.filter((person) => {
-        switch (rule.operator) {
-          case 'equals':
-            return person[rule.propertyName] === rule.value;
-          case 'matches_regex':
-            validateRegexRule(rule, person);
-            return new RegExp(rule.value as string).test(person[rule.propertyName] as string);
+    async fetchPeople(swApiUrl: string): Promise<SwPerson[]> {
+      const fetchPage = (pageUrl: string) => axios.get(pageUrl);
+      const processPage = async ({ data: pageResult }: AxiosResponse<SwResponse>): Promise<SwPerson[]> => {
+        // loading everything to memory with recursive calls
+        // there are 82 items in SW api, so that's ok, but with more data we should persist it to some external storage instead
+        // (and refactor to iterative manner)
+        if (pageResult.next) {
+          return [...pageResult.results, ...(await fetchPage(pageResult.next).then(processPage))];
         }
-      }),
-    people
-  );
+        return pageResult.results;
+      };
+      const results = await fetchPage(`${swApiUrl}/people`).then(processPage);
+      return results;
+    },
+
+    async performFiltering(people: SwPerson[], rules: SearchRule<SwPerson>[]): Promise<SwPerson[]> {
+      return rules.reduce(
+        (filteredPeople, rule) =>
+          filteredPeople.filter((person) => {
+            switch (rule.operator) {
+              case 'equals':
+                return person[rule.propertyName] === rule.value;
+              case 'matches_regex':
+                validateRegexRule(rule, person);
+                return new RegExp(rule.value as string).test(person[rule.propertyName] as string);
+            }
+          }),
+        people
+      );
+    },
+
+    async fetchAndFilterPeople(swApiUrl: string, rules: SearchRule<SwPerson>[]): Promise<void> {
+      const request: GetPeopleRequest = {
+        rules,
+        initiatorId: activityInfo().workflowExecution.workflowId,
+      };
+      await client.workflow.signalWithStart('getPeople', {
+        taskQueue: activityInfo().taskQueue,
+        args: [swApiUrl],
+        workflowId: `get-people`,
+        signal: getPeopleSignal,
+        signalArgs: [request],
+      });
+    },
+  };
 }
 
 function validateRegexRule<Entity>(rule: SearchRule<Entity>, entity: Entity): void {
